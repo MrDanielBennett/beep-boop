9d0cde47a90467f0268279b2942b3ff9b574189f
MrDanielBennett <daniel.bennett1999@gmail.com>
2018-10-26 10:57:35 -0700

    create function that will cycle through each number in array and check for a certain input and then if found replace it with certain output

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 //Back end
 function createInputArray(input){
-  debugger
   var inputArray = [];
   for (i = 0; i <= input; i++){
     inputArray.push(i);
   }
-
+  return beepBoop(inputArray)
 }
 
-function beepBoop(){
-  createInputArray();
+function beepBoop(inputArray){
+  debugger
+  var output = []
   for (i=0; i<inputArray.length; i++){
-    if ([i] === 0){
+    if (inputArray[i] === 0){
       inputArray[i] = "Beep";
     }
+    if (inputArray[i] === 1){
+        inputArray[i] = "Boop";
+    }
+    if (inputArray[i] %3 === 0){
+      inputArray[i] = "I'm sorry Dave. I'm afraid I can't do that.";
+    }
+    console.log(inputArray)
   }
 }
 
